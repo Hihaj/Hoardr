d5890255a6d03cae4a47dd6c042f4100ed64fc96
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2015-05-17 23:50:02 +0200

    Added Dropbox settings.

--- src/Hoardr.Api/Dropbox/DropboxSettings.cs
+++ src/Hoardr.Api/Dropbox/DropboxSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -8,5 +9,12 @@ namespace Hoardr.Api.Dropbox
     public class DropboxSettings
     {
         public string ApiSecret { get; set; }
+        public string AccessToken { get; set; }
+
+        public DropboxSettings()
+        {
+            ApiSecret = ConfigurationManager.AppSettings["dropbox:ApiSecret"];
+            AccessToken = ConfigurationManager.AppSettings["dropbox:AccessToken"];
+        }
     }
 }
