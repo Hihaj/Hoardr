8b1ee1e4b79a3e9cac0d2c773563ec8062ec0363
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2015-05-20 11:50:39 +0200

    Added a console app that will be the file download worker.

---
+++ src/Hoardr.FileJob/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoardr.FileJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
